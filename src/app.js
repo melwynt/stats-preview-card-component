c6c3f0ffb3cae1816823f8c620e0185e79bec334
Melwyn Turbant <melwynt@gmail.com>
2022-03-14 22:59:45 +0100

    :sparkles: Create Desktop Design

--- src/app.js
+++ src/app.js
@@ -3,31 +3,45 @@ import React, { useState } from 'react';
 const App = () => {
   return (
     <main>
-      <div className="left">
-        <h1>
-          Get <span>insights</span> that help your business grow.
-        </h1>
-        <p>
-          Discover the benefits of data analytics and make better decisions
-          regarding revenue, customer experience and overall efficiency.
-        </p>
-        <div className="metrics">
-          <div className="metric first">
-            <h2>10k+</h2>
-            <h3>Companies</h3>
-          </div>
-          <div className="metric">
-            <h2>314</h2>
-            <h3>Templates</h3>
-          </div>
-          <div className="metric">
-            <h2>12M+</h2>
-            <h3>Queries</h3>
+      <div className="card">
+        <div className="left-panel">
+          <h1>
+            Get <span>insights</span> that help your business grow.
+          </h1>
+          <p className="left-panel__description">
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div className="left-panel__metrics-container">
+            <div className="left-panel__metrics-container__metric first">
+              <p className="left-panel__metrics-container__metric__stats">
+                10k+
+              </p>
+              <p className="left-panel__metrics-container__metric__heading">
+                Companies
+              </p>
+            </div>
+            <div className="left-panel__metrics-container__metric">
+              <p className="left-panel__metrics-container__metric__stats">
+                314
+              </p>
+              <p className="left-panel__metrics-container__metric__heading">
+                Templates
+              </p>
+            </div>
+            <div className="left-panel__metrics-container__metric last">
+              <p className="left-panel__metrics-container__metric__stats">
+                12M+
+              </p>
+              <p className="left-panel__metrics-container__metric__heading">
+                Queries
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="right">
-        <img alt="happy people" />
+        <div className="right-panel">
+          <img alt="" className="right-panel__image" />
+        </div>
       </div>
     </main>
   );
