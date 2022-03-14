59cbb143165bdac7a6b3b783f4bf6ce5f9d001a3
Melwyn Turbant <melwynt@gmail.com>
2022-03-14 15:43:35 +0100

    :tada: Create raw HTML

--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <main>
+      <div className="left">
+        <h1>
+          Get <span>insights</span> that help your business grow.
+        </h1>
+        <p>
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience and overall efficiency.
+        </p>
+        <div className="metrics">
+          <div className="metric first">
+            <h2>10k+</h2>
+            <h3>Companies</h3>
+          </div>
+          <div className="metric">
+            <h2>314</h2>
+            <h3>Templates</h3>
+          </div>
+          <div className="metric">
+            <h2>12M+</h2>
+            <h3>Queries</h3>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img alt="happy people" />
+      </div>
+    </main>
+  );
 };
 
 export default App;
