d299a53b9d0716bdf4f94a7c2c35696ba4dd69d8
Melwyn Turbant <melwynt@gmail.com>
2022-03-15 14:19:54 +0100

    :rocket: Update Desktop and Mobile design

--- src/app.js
+++ src/app.js
@@ -3,46 +3,36 @@ import React, { useState } from 'react';
 const App = () => {
   return (
     <main>
-      <div className="card">
-        <div className="left-panel">
-          <h1>
-            Get <span>insights</span> that help your business grow.
-          </h1>
-          <p className="left-panel__description">
-            Discover the benefits of data analytics and make better decisions
-            regarding revenue, customer experience, and overall efficiency.
-          </p>
-          <div className="left-panel__metrics-container">
-            <div className="left-panel__metrics-container__metric first">
-              <p className="left-panel__metrics-container__metric__stats">
-                10k+
-              </p>
-              <p className="left-panel__metrics-container__metric__heading">
-                Companies
-              </p>
+      <section className="stats-card">
+        <div className="stats-card__hero-info">
+          <div className="stats-card__hero-info__description">
+            <h1>
+              Get <span>insights</span> that help your business grow.
+            </h1>
+            <p>
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>
+          </div>
+          <div className="stats-card__hero-info__data">
+            <div className="stats-card__hero-info__data__box">
+              <p>10k+</p>
+              <p>companies</p>
             </div>
-            <div className="left-panel__metrics-container__metric">
-              <p className="left-panel__metrics-container__metric__stats">
-                314
-              </p>
-              <p className="left-panel__metrics-container__metric__heading">
-                Templates
-              </p>
+            <div className="stats-card__hero-info__data__box">
+              <p>314</p>
+              <p>templates</p>
             </div>
-            <div className="left-panel__metrics-container__metric last">
-              <p className="left-panel__metrics-container__metric__stats">
-                12M+
-              </p>
-              <p className="left-panel__metrics-container__metric__heading">
-                Queries
-              </p>
+            <div className="stats-card__hero-info__data__box">
+              <p>12M+</p>
+              <p>queries</p>
             </div>
           </div>
         </div>
-        <div className="right-panel">
-          <img alt="" className="right-panel__image" />
+        <div className="stats-card__hero-img">
+          <img alt="" />
         </div>
-      </div>
+      </section>
     </main>
   );
 };
